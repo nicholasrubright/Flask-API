e0bf83749f738f2c91715ee091d288904ea07083
Nicholas Rubright <koalrube@gmail.com>
2023-08-07 12:16:26 -0400

    got docker support working

--- db/init.sql
+++ db/init.sql
@@ -1,23 +1,11 @@
--- CREATE DATABASE movie_matcher;
-
--- \c movie_matcher postgres;
-
--- CREATE TABLE IF NOT EXISTS MMSessions (
---     id INT NOT NULL,
---     code TEXT NOT NULL,
---     match BOOLEAN NOT NULL,
---     movie_match_id INT,
-
---     PRIMARY KEY (id)
--- );
-
--- INSERT INTO MMSessions
--- values (1, 'testcode', false, NULL);
-
 CREATE DATABASE MovieDatabase;
 
 \c MovieDatabase postgres;
 
 CREATE TABLE IF NOT EXISTS Movies (
-    id 
-)
+    id INT NOT NULL,
+    title VARCHAR(50) NOT NULL
+);
+
+INSERT INTO Movies
+VALUES (1, 'Test Movie');
